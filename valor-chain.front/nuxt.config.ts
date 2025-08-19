0f345f0219e9fdf19795c5edadf3b5f1e61ebb94
idoumbouya <doumbouyaibrahima@gmail.com>
2025-08-19 18:11:30 +0000

    initiation de @workmate/nuxt-auth et @nuxtjs/dotenv

--- valor-chain.front/nuxt.config.ts
+++ valor-chain.front/nuxt.config.ts
@@ -2,5 +2,45 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: [    
+    // https://go.nuxtjs.dev/axios
+    //'@nuxtjs/axios',
+    // https://go.nuxtjs.dev/i18n
+    '@nuxtjs/i18n',
+    // https://auth.nuxtjs.org/
+    //'@nuxtjs/auth-next',
+    '@workmate/nuxt-auth',
+    // https://github.com/nuxt-community/dotenv-module
+    '@nuxtjs/dotenv',
+  ],
+  /*axios: {
+    // Base URL pour vos requêtes API. Sera surchargée par les variables d'environnement.
+    baseURL: process.env.API_URL || 'http://localhost:8000/api',
+  },
+  auth: {
+    strategies: {
+    local: {
+      token: {
+        property: 'access_token',
+        global: true,
+        required: true,
+        type: 'Bearer'
+      },
+      user: {
+        property: 'user',
+        autoFetch: true
+      },
+      endpoints: {
+        login: { url: '/auth/login', method: 'post' },
+        logout: { url: '/auth/logout', method: 'post' },
+        user: { url: '/auth/user', method: 'get' }
+      }
+    }
+    }
+    },
+    // Configuration du module dotenv
+    dotenv: {
+      path: './',
+      filename: '.env'
+    },*/
 })
